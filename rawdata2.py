3b13812c50f1ec82ebade62fa0f71728dcb16519
Iyad Omayr Hassan <57540443+youngyad@users.noreply.github.com>
2023-06-08 00:52:20 -0700

    Update rawdata2.py

--- rawdata2.py
+++ rawdata2.py
@@ -17,7 +17,7 @@ class State:
         self.steady = 0
         self.lr = False
 
-gesture_data = []
+gesture_data = {}
 
 def startServer(local_ip, local_port, buffer_size=2048):
     global gesture_data
@@ -26,19 +26,20 @@ def startServer(local_ip, local_port, buffer_size=2048):
     udp_server_socket.bind((local_ip, local_port))
     print("Done.                                -1/2")
 
-    # Prepare motion data for a specific gesture
-    state = State()
-
-    # Listen for incoming datagrams
     while True:
-        bytes_address_pair = udp_server_socket.recvfrom(buffer_size)
-        message = bytes_address_pair[0]
+        gesture_label = input("Enter the label for the gesture (or 'q' to quit): ")
+        if gesture_label.lower() == 'q':
+            break
 
-        # Process motion data
-        update_state(message, state)
+        state = State()
+        gesture_data[gesture_label] = []
 
-        # Store motion data for the specific gesture
-        gesture_data.append(state_to_dict(state))
+        while True:
+            bytes_address_pair = udp_server_socket.recvfrom(buffer_size)
+            message = bytes_address_pair[0]
+            update_state(message, state)
+            gesture_data[gesture_label].append(state_to_dict(state))
+
 
 def update_state(m, state):
     if m.startswith(b'jolt'):
@@ -89,8 +90,6 @@ def update_state(m, state):
         if state.qe_rot != temp:
             state.qe_rot = temp
 
-def udp_int_decode(i):
-    return struct.unpack('!I', i[-4:])[0]
 
 def state_to_dict(state):
     return {
@@ -108,28 +107,30 @@ def state_to_dict(state):
         "lr": state.lr
     }
 
-def save_data_to_file(data, file_path):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4)
-        print("Motion data saved to file:", file_path)
 
 if __name__ == "__main__":
     localIP = "127.0.0.1"
     localPort = 4010
-    output_file = "motion_data.json"
 
     try:
         startServer(localIP, localPort)
     except KeyboardInterrupt:
-        # Load existing gestures from the JSON file
+        output_file = "gesture_data.json"
         try:
+            # Load existing gesture data from the JSON file
             with open(output_file, 'r') as file:
                 existing_data = json.load(file)
         except FileNotFoundError:
-            existing_data = []
+            existing_data = {}
 
-        # Append new gestures to the existing data
-        existing_data.extend(gesture_data)
+        # Extend the existing gesture data with the new data
+        for label, data in gesture_data.items():
+            if label in existing_data:
+                existing_data[label].extend(data)
+            else:
+                existing_data[label] = data
 
-        # Save the updated data to the JSON file
-        save_data_to_file(existing_data, output_file)
+        # Save the updated gesture data to the JSON file
+        with open(output_file, 'w') as file:
+            json.dump(existing_data, file, indent=4)
+            print("Gesture data saved to file:", output_file)
